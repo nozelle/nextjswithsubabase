1d82b75ef6add7d3fbf7811418f2685bfa373bf6
HAREN PRABU <80617267+harenprabu@users.noreply.github.com>
2023-09-04 14:16:22 +0530

    Add files via upload

---
+++ src/app/dashborad/page.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+import SignOut from 'src/components/SignOut';
+
+export default async function Profile() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect('/sign-in');
+  }
+
+  return (
+    <div className="card">
+      <h1>Dashboard</h1>
+      <h1>welcome,{user.email}</h1>
+      <code className="highlight">{user.email}</code>
+      <div className="heading">Last Signed In:</div>
+      <code className="highlight">{new Date(user.last_sign_in_at).toUTCString()}</code>
+      <Link className="button" href="/">
+        Go Home
+      </Link>
+    
+      <SignOut />
+    </div>
+    
+  );
+}
