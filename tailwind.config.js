5c096b2ee4fc049bc1475c3fdcb5071dfa63d5c8
HAREN PRABU <80617267+harenprabu@users.noreply.github.com>
2023-09-05 19:53:58 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import {nextui} from "@nextui-org/react";
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+   
     './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
 };
